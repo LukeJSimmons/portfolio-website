a445dc26b6520555427d14b3cb38532ce979b296
LukeJSimmons <lucasjsimmons@yahoo.com>
2024-06-13 16:00:26 -0500

    Fix mobile header and banner

--- src/components/HeaderSection/HeaderSection.js
+++ src/components/HeaderSection/HeaderSection.js
@@ -1,41 +1,46 @@
 import './HeaderSection.css';
 
-import React from "react";
+import React, { useState } from "react";
 
 import moreLines from '../../images/WEBP/MoreLines.webp';
 
 const HeaderSection = () => {
 
-    let prevScrollPos = window.scrollY;
-
-    window.onscroll = function() {
-        const currentScrollPos = window.scrollY;
-        const header = document.querySelector('header');
-
-        if (prevScrollPos > currentScrollPos) {
-            header.style.top = '0';
-        } else {
-            header.style.top = '-80px';
-        }
-
-        prevScrollPos = currentScrollPos;
-    }
-
-    const navScreen = document.getElementById("navScreen");
+    const [navScreenShow, setNavScreenShow] = useState(false);
 
     function handleNavBtnClick() {
-        var displayValue = window.getComputedStyle(navScreen).getPropertyValue("display");
-        navScreen.style.display = (displayValue === "none") ? "flex" : "none";
+        setNavScreenShow(!navScreenShow);
+    }
+
+    let prevScrollPos = window.scrollY;
+
+    if (!navScreenShow) {
+        window.onscroll = function() {
+            const currentScrollPos = window.scrollY;
+            const header = document.querySelector('header');
+    
+            if (prevScrollPos > currentScrollPos) {
+                header.style.top = '0';
+            } else {
+                header.style.top = '-80px';
+            }
+    
+            prevScrollPos = currentScrollPos;
+        }
+    } else {
+        window.onscroll = function() {
+            return;
+        }
     }
     
     return (
         <header>
-          <div id="navScreen" className="flex">
+          {navScreenShow ? (<div id="navScreen" className="flex">
               <a href="#banner">Home</a><br/>
               <a href="#portfolio">Portfolio</a><br/>
               <a href="#about">About Me</a><br/>
               <a href="#contact">Contact</a>
-          </div>
+          </div>) : (<></>)}
           <ul className="flex">
               <li><a href="#banner">Home</a></li>
               <li><a href="#portfolio">Portfolio</a></li>
