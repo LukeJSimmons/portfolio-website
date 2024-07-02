8b13d379a1c92940e2f3a7f151322a614dfd3a4e
LukeJSimmons <lucasjsimmons@yahoo.com>
2024-07-02 10:19:54 -0500

    Make title text instead of img

--- src/components/BannerSection/BannerSection.js
+++ src/components/BannerSection/BannerSection.js
@@ -7,7 +7,10 @@ import bannerTitle from '../../images/WEBP/WebsiteBannerTitle.webp';
 const BannerSection = () => {
     return (
         <div id="banner" className="flex">
-            <img src={bannerTitle} alt='Luke Simmons: Coder, Designer, Podcaster'/>
+            <h1 className='bowlby-one-sc-regular'><em>Luke Simmons</em></h1>
+            <hr className='rounded' />
+            <h4 className='bowlby-one-sc-regular'>Coder ~ Designer ~ Podcaster</h4>
+            {/* <img src={bannerTitle} alt='Luke Simmons: Coder, Designer, Podcaster'/> */}
         </div>
     );
 };
